094d11dfd6fd426fa4b31dee1779ec81d279c6c2
Sérgio Miguel Carvalho Gonçalves <up201603271@fe.up.pt>
2020-03-04 15:26:36 +0000

    Update listaligada.c
    Falta o b)

--- Pratica/listaligada.c
+++ Pratica/listaligada.c
@@ -1,6 +1,9 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <fcntl.h>
 
 // Defini��o da estrutura da lista
 typedef struct{
@@ -9,6 +12,8 @@ typedef struct{
 	struct lista * proximo;
 } lista;
 
+float median(float *vetor,int size);
+int save(int *histograma,int size);
 
 // Fun��o que remove o primeiro elemento da lista
 lista * remover (lista * apontador)
@@ -77,29 +82,107 @@ void imprimir (lista * apontador)
 	}
 }
 
-// Pequeno exemplo de utiliza��o
-int main(int argc, char* argv[])
-{
-	lista  * lista_eventos;
-	int tipo_ev = 1; double tempo_ev;
-	int lambda = atoi(argv[1]);
-	int k = atoi(argv[2]);
-	float u,c;
-	srand((unsigned)(time(NULL)));
-	//tipo 1 chegada
-	lista_eventos = NULL;
+// // Pequeno exemplo de utiliza��o
+// int main(int argc, char* argv[])
+// {
+// 	lista  * lista_eventos;
+// 	int tipo_ev = 1; double tempo_ev;
+// 	int lambda = atoi(argv[1]);
+// 	int k = atoi(argv[2]);
+// 	float u,c,td[k];
+// 	srand((unsigned)(time(NULL)));
+// 	//tipo 1 chegada
+// 	lista_eventos = NULL;
+//
+// 	for (int i = 0; i < k; i++){
+// 		u=((float)rand())/RAND_MAX;
+// 		c = -(1.0/lambda)*log(u);
+// 		lista_eventos = adicionar(lista_eventos, tipo_ev, c);
+// 		if(i > 1){
+// 			// lista_eventos = remover(lista_eventos);
+// 			// printf("Elemento removido\n");
+// 		}
+// 	}
+//
+// 	printf("\nLISTA ACTUAL\n");
+// 	imprimir(lista_eventos);
+//
+// }
 
-	for (int i = 0; i < k; i++){
-		u=((float)rand())/RAND_MAX;
-		c = -(1.0/lambda)*log(u);
-		lista_eventos = adicionar(lista_eventos, tipo_ev, u);
-		if(i > 1){
-			// lista_eventos = remover(lista_eventos);
-			// printf("Elemento removido\n");
-		}
-	}
+int main(int argc, char* argv[]){
 
-	printf("\nLISTA ACTUAL\n");
+   size_t t;
+   int lambda,k;
+	 lista  * lista_eventos;
+	 lista_eventos = NULL;
+   lambda = atoi(argv[1]);
+   k = atoi(argv[2]);
+   float delta;
+   delta = (float)(1.0/lambda);// valor max para delta
+   delta *=(float)1/5;
+   printf("Valor do delta:%.2f\n",delta);
+
+    srand((unsigned)(time(NULL)));
+    float *vetor= malloc(sizeof(long)*(k+2));
+    int *histograma=malloc(sizeof(long)*(k+2));
+
+    memset(histograma,0,0);
+    memset(vetor,0,0);
+
+  for (int i = 0; i < k; i++)
+  {
+      float u=((float)rand())/RAND_MAX;
+      float c = -(1.0/lambda)*log(u);
+      vetor[i]= (float) c;
+			lista_eventos = adicionar(lista_eventos, 1, c);
+      printf("%dº /-/ %f /-/ %f\n", i+1, u, c);
+  }
 	imprimir(lista_eventos);
 
+  int N= 1/delta;
+  printf("Valor de N:%d\n",N);
+	float media = median(vetor,k);
+	printf("Média de tempos entre chegadas de chamadas : %f\n",media);
+
+  for (int j = 0; j < k; j++)
+  {
+    for (int n = 0;n < N;n++){
+      if((vetor[j] >= n*delta) && (vetor[j] < (n+1)*delta)){
+        histograma[n]++;
+      }
+      //printf("Valor do vetor para %d:%f\n",j,vetor[j]);
+      //printf("Valor do histograma para %d:%d\n",j,histograma[j]);
+    }
+  }
+
+  for (int j = 0; j < N; j++)
+  {
+      printf("Valor do histograma para %dº intervalo :%d\n",j,histograma[j]);
+  }
+
+  save(histograma,N);
+
+}
+
+float median(float *vetor,int size){
+	float soma;
+	for(int i=0;i < size;i++){
+		soma+=vetor[i];
+	}
+	return (soma/size);
+}
+
+int save(int *histograma,int size){
+  FILE* file;
+  file= fopen("call.csv","w+");
+
+  if(file == NULL){
+    printf("Erro a abrir o ficheiro\n");
+  }
+
+  for (int j = 0; j < size; j++)
+  {
+      fprintf(file, "%d, %lf, %d\n", j, (2*j+1)/(float)(size*2), histograma[j]);
+      //printf("%d, %lf, %d\n", j, (2*j+1)/(float)(size*2), histograma[j]);
+  }
 }
