1c09dfdfb3d59dbdbb00b839396002227c684f07
Sérgio Miguel Carvalho Gonçalves <up201603271@fe.up.pt>
2020-03-23 14:47:25 +0000

    Update 3a.c
    Listas! capacidade:1

--- Pratica/3a.c
+++ Pratica/3a.c
@@ -5,6 +5,8 @@
 #include <string.h>
 #include <fcntl.h>
 #define DELTA 1E-3
+#define ARRIVAL 0
+#define DEPARTURE 1
 
 // Defini��o da estrutura da lista
 typedef struct{
@@ -86,12 +88,13 @@ void imprimir (lista * apontador)
 int main(int argc, char* argv[]){
 
    size_t t;
+	 char type;
    int lambda,k,num=0,Ngrande,busy,blocked;
 	 lista  * lista_eventos;
 	 lista_eventos = NULL;
    lambda = atoi(argv[1]);
-   float p,n;
    k = atoi(argv[2]);
+	 Ngrande = atoi(argv[3]);
    float delta,u,d,dm=0.008; // miu=1/dm=125 k
    delta = (float)(1.0/lambda);// valor max para delta
    delta *=(float)1/5;
@@ -102,37 +105,57 @@ int main(int argc, char* argv[]){
 	 float *duracao= malloc(sizeof(double)*(k+2));
    int *histograma=malloc(sizeof(double)*(k+2));
 
-   p= DELTA*lambda;
-	 float current,pblocking;
-   printf("Valor da probabilidade:%.4f\n",p);
+	 float current=0.0,pblocking;
 
    memset(histograma,0,0);
    memset(vetor,0,0);
 	 memset(duracao,0,0);
+	 float c=0.0;
+	 lista_eventos = adicionar(lista_eventos, ARRIVAL, 0);
 
   for (int i = 0; i < k; i++)
   {
-		 float u=((float)rand())/RAND_MAX;
-		 float c = -(1.0/lambda)*log(u); //intervalo entre esta e aprox chamada
-		 u=((float)rand())/RAND_MAX;
-		 d= -dm*log(u); //duração da chamada
-		 //current+=c;
-		 lista_eventos = adicionar(lista_eventos, 0, c); // atendimento
-		 if(d < c){
-			 current+=d+c;
-		 }
-		 else if (d >= c){
-			 ++blocked;
-			 busy=1;
-			 current+=d;
-			 c = -(1.0/lambda)*log(u);
-			 current+=c;
+		 if(busy < 0){
 			 busy=0;
 		 }
+
+		 float u=((float)rand())/RAND_MAX;
+		 c = -(1.0/lambda)*log(u); //intervalo entre esta e aprox chamada
+		 u=((float)rand())/RAND_MAX;
+		 d= -dm*log(u); //duração da chamada
+
+		 lista_eventos = adicionar(lista_eventos, ARRIVAL, current+c); // atendimento
+		 lista_eventos = adicionar(lista_eventos, DEPARTURE, current+d); // fim de atendimento
+		 //imprimir(lista_eventos);
+
+		 if(lista_eventos->tipo == DEPARTURE){ //fim de chamada
+			 current+=d;
+			 busy--;
+			 lista_eventos = remover(lista_eventos);
+		 }
+
+		 else if (lista_eventos->tipo == ARRIVAL ){ //inicio de chamada
+			 	 busy++;
+				 if(busy > Ngrande){ //recursos livres? Não
+					 ++blocked;
+					 lista_eventos = remover(lista_eventos);
+					 lista_eventos = adicionar(lista_eventos, ARRIVAL, current); // atendimento
+					 // u=((float)rand())/RAND_MAX;
+					 // c = -(1.0/lambda)*log(u);
+				 }
+
+				 // else if(busy <= Ngrande){ //recursos livres? sim
+					//  lista_eventos = remover(lista_eventos);
+				 // }
+				 else{
+					 lista_eventos = remover(lista_eventos);
+					 current+=c;
+				 }
+		 	}
+
 	   vetor[i]= (float) c;
 		 duracao[i]= (float) d;
-		 lista_eventos = adicionar(lista_eventos, 1, d); // fim de atendimento
-	   printf("%dº /-/ %f /-/ %f /-/ %f\n", i+1, d, c,current);
+	   //printf("%dº /-/ %f /-/ %f /-/ %f\n", i+1, d, c,current);
   }
 
 	//imprimir(lista_eventos);
@@ -163,6 +186,9 @@ int main(int argc, char* argv[]){
   }
 
   save(histograma,N);
+	free(histograma);
+	free(duracao);
+	free(vetor);
 
 }
 
